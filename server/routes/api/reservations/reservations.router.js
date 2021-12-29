a9200dc016e8a871c859310fb882927a468041a5
Infamou5 <oreal_b52@hotmail.com>
2021-12-29 12:23:08 +0100

    Added more routes to reservations

--- server/routes/api/reservations/reservations.router.js
+++ server/routes/api/reservations/reservations.router.js
@@ -1,22 +1,13 @@
 const router = require("express").Router();
-const { validate_meal } = require("../../../validators/meals/meals.validator");
-const { get_meal_by_id } = require("../../../controllers/meals/get_meal_by_id");
-const { create_new_meal } = require("../../../controllers/meals/create_new_meal");
-const { get_all_meals_filtered } = require("../../../controllers/meals/get_all_meals_filtered");
-const { delete_meal_by_id } = require("../../../controllers/meals/delete_meal_by_id");
-const { get_all_reviews_by_meal_id } = require("../../../controllers/meals/get_all_reviews_by_meal_id");
-const { get_all_reservations_by_meal_id } = require("../../../controllers/meals/get_all_reservations_by_meal_id");
+const { validate_reservation } = require("../../../validators/reservations/reservations.validator");
+const { create_new_reservation } = require("../../../controllers/reservations/create_new_reservation");
+const { get_reservation_by_id } = require("../../../controllers/reservations/get_reservation_by_id");
+const { get_all_reservations_filtered } = require("../../../controllers/reservations/get_all_reservations_filtered");
+const { delete_reservation_by_id } = require("../../../controllers/reservations/delete_reservation_by_id");
 
-router.get("/:id/reviews", validate_meal("get_all_reviews_by_meal_id"), get_all_reviews_by_meal_id )
-
-router.get("/:id/reservations", validate_meal("get_all_reservations_by_meal_id"), get_all_reservations_by_meal_id )
-
-router.get("/", validate_meal("get_all_meals_filtered"), get_all_meals_filtered); // COMPLETED
-
-router.get("/:id", validate_meal("get_meal_by_id"), get_meal_by_id); // COMPLETED
-
-router.delete("/:id", validate_meal("delete_meal_by_id"), delete_meal_by_id); // COMPLETED
-
-router.post("/", validate_meal("create_new_meal"), create_new_meal); // COMPLETED
+router.get("/:id", validate_reservation("get_reservation_by_id"), get_reservation_by_id); // COMPLETED
+router.get("/", validate_reservation("get_all_reservations_filtered"), get_all_reservations_filtered); // COMPLETED
+router.post("/", validate_reservation("create_new_reservation"), create_new_reservation); // COMPLETED
+router.delete("/:id", validate_reservation("delete_reservation_by_id"), delete_reservation_by_id); // COMPLETED
 
 module.exports = router;
--- server/validators/reservations/reservations.validator.js
+++ server/validators/reservations/reservations.validator.js
@@ -1,22 +1,10 @@
 const { check, body, param, validationResult } = require("express-validator");
 
-exports.validate_meal = (method) => {
+exports.validate_reservation = (method) => {
   switch (method) {
-    case "create_new_review": {
+    case "create_new_reservation": {
       return [
-        body("title", "title doesn't exists").exists().trim().escape(),
-        body("description", "description does not exist").exists().escape().trim(),
-        body("location", "location must be a string").exists().trim().escape().isString(),
-        body("price", "price must be a number").exists().trim().escape().isInt(),
-        body("max_seats", "max_seats must be a number")
-          .exists()
-          .withMessage("You must provide this info.")
-          .trim()
-          .escape()
-          .toInt(),
-        body("available_seats")
-          .exists().withMessage("You must provide this value")
-          .isInt().withMessage("Available seats must be a number."),
+
       ];
     }
   }
